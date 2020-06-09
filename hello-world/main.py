e67a77d3c8dbf89c8ea75d36b69171b94f9b2884
Igor Cavalcanti <igor.lima@luizalabs.com>
2020-06-08 21:49:06 -0300

    adding path parameter

--- hello-world/main.py
+++ hello-world/main.py
@@ -15,5 +15,9 @@ def hello_guest(q: str = "Guest"):
     return {"Hello": q}
 
 
+@app.get("/hello/{name}")
+async def read_item(name):
+    return {"Hello": name}
+
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
