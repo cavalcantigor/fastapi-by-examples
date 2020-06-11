4a42fc286b4fdd0d73cb77845e243dc8d37fa750
Igor Cavalcanti <igor.lima@luizalabs.com>
2020-06-10 23:23:40 -0300

    adding post method

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,9 +1,17 @@
 from typing import List
 from fastapi import (FastAPI, Query, status)
+from pydantic import BaseModel
 import uvicorn
 
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
 
 
 @app.get("/", status_code=200)
@@ -24,6 +32,11 @@ async def hello_validated_guest(q: str = Query("Guest", max_length=10)):
 @app.get("/hello/{name}", status_code=status.HTTP_200_OK)
 async def read_item(name):
     return {"Hello": name}
+
+
+@app.post("/item/")
+async def create_item(item: Item):
+    return item
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
