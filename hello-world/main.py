715d29c0c2c348fd200159db51ac4449c847abd0
Igor Cavalcanti <igor.lima@luizalabs.com>
2020-06-10 23:26:05 -0300

    adding nested model

--- hello-world/main.py
+++ hello-world/main.py
@@ -7,11 +7,17 @@ import uvicorn
 app = FastAPI()
 
 
+class Image(BaseModel):
+    url: str
+    name: str
+
+
 class Item(BaseModel):
     name: str
     description: str = None
     price: float
     tax: float = None
+    image: Image
 
 
 @app.get("/", status_code=200)
@@ -37,6 +43,12 @@ async def read_item(name):
 @app.post("/item/")
 async def create_item(item: Item):
     return item
+
+
+@app.put("/item/{item_id}")
+async def update_item(*, item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
